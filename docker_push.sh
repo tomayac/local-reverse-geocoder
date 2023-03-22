d36d768470d6a92eab458dbe5b920a7775b5dc60
Nathan Humble <nhumble@secureauth.com>
2023-03-22 09:40:03 -0700

    Bump version for security updates

--- docker_push.sh
+++ docker_push.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# docker build . -t "064061306967.dkr.ecr.us-west-2.amazonaws.com/acceptto/local-reverse-geocoder:1.0.0"
-# docker push 064061306967.dkr.ecr.us-west-2.amazonaws.com/acceptto/local-reverse-geocoder:1.0.0
+VERSION=1.2.2
 
-docker buildx build --platform linux/amd64,linux/arm64/v8 --push -t 064061306967.dkr.ecr.us-west-2.amazonaws.com/acceptto/local-reverse-geocoder:1.2.0 .
+docker buildx build --platform linux/amd64,linux/arm64/v8 --push -t 064061306967.dkr.ecr.us-west-2.amazonaws.com/acceptto/local-reverse-geocoder:$VERSION .

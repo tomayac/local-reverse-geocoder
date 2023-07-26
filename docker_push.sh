8cdc8cf546a2a99a6c679683013b0259205635b4
Nathan Humble <nhumble@secureauth.com>
2023-07-26 16:21:35 -0700

    Bump version for security rebuild.
    Also prevent zero byte artifacts on ECR.

--- docker_push.sh
+++ docker_push.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-VERSION=1.2.2
+VERSION="1.2.4"
 
-docker buildx build --platform linux/amd64,linux/arm64/v8 --push -t 064061306967.dkr.ecr.us-west-2.amazonaws.com/acceptto/local-reverse-geocoder:$VERSION .
+docker buildx build --sbom=false --provenance=false --platform linux/amd64,linux/arm64/v8 --push -t 064061306967.dkr.ecr.us-west-2.amazonaws.com/acceptto/local-reverse-geocoder:$VERSION .

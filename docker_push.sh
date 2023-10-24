f40b2096b1f2cb4288125fd6a6747a3770fc8838
Nathan Humble <nhumble@secureauth.com>
2023-10-24 16:00:23 -0700

    Bump version for rebuild.

--- docker_push.sh
+++ docker_push.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-VERSION="1.2.4"
+VERSION="1.2.5"
 
 docker buildx build --sbom=false --provenance=false --platform linux/amd64,linux/arm64/v8 --push -t 064061306967.dkr.ecr.us-west-2.amazonaws.com/acceptto/local-reverse-geocoder:$VERSION .

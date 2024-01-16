ce5c532d141f1e8b58d47067657ba95cc923eeed
Nathan Humble <nhumble@secureauth.com>
2024-01-16 14:18:43 -0800

    Pull version from git tag.

--- docker_push.sh
+++ docker_push.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-VERSION="1.2.5"
+set -e
+
+VERSION="$(git describe --tags --match 'v*')"  # Get version tag from git
+VERSION=${VERSION#v}                           # Remove leading 'v'
+
+read -p "This action will build and publish docker image version $VERSION (this will automatically proceed in 15 seconds)" -t 15
 
 docker buildx build --sbom=false --provenance=false --platform linux/amd64,linux/arm64/v8 --push -t 064061306967.dkr.ecr.us-west-2.amazonaws.com/acceptto/local-reverse-geocoder:$VERSION .
